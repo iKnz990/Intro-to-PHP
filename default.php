2ef94074f1835ac7d16c1dee00fc91e17a3f843c
iknz <alexander@alexandernoctis.com>
2023-08-23 14:22:15 -0600

    homepage

---
+++ default.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>WDV341 Class Assignments</title>
+        <link rel="icon" type="image/x-icon" href="favicon.png">
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="WDV341 Class Assignments" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+        <style>
+            body {
+                margin: 0px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                width: 100vw;
+                height: 100vh;
+                min-height: 675px;
+                background-color: #F4F5FF;
+            }
+            p {
+                width: 100%;
+                left: 0px;
+                font-size: 16px;
+                font-family: 'DM Sans', sans-serif;
+                font-weight: 400;
+                letter-spacing: 0px;
+                text-align: center;
+                vertical-align: top;
+                max-width: 550px;
+                color: #727586;
+                margin: 0px;
+            }
+            a:hover {
+                cursor: pointer;
+                color: #673DE6;
+                text-decoration: underline;
+            }
+            h1 {
+                font-family: 'DM Sans', sans-serif;
+                font-size: 24px;
+                font-weight: 700;
+                letter-spacing: 0px;
+                text-align: center;
+                margin: 8px;
+            }
+            .content {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                width: 100%;
+                height: 100%;
+            }
+            .navigation {
+                width: 100%;
+                height: 72px;
+                display: flex;
+                margin: 0;
+                padding: 0;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                background-color: #990003;
+            }
+            @media screen and (max-width: 580px) and (min-width: 0px) {
+                h1, p {
+                    width: 80%;
+                }
+            }
+            @media screen and (min-width: 650px) and (min-height: 0px) and (max-height: 750px) {
+                h1 {
+                    margin-top: 0px;
+                    margin-bottom: 0px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navigation">
+            <a href="#" rel="nofollow" target="_blank">
+                <h1>Assignments Go Here... Eventually</h1>
+            </a>
+        </nav>
+        <div class="content">
+            <h1>Welcome to WDV341 Class Assignments</h1>
+            <p>Here you will find all the assignments and resources for WDV341. Feel free to explore and reach out if you have any questions.</p>
+            <!-- Add links to assignments and resources here . -->
+        </div>
+    </body>
+</html>
